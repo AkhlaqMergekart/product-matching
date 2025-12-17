618ee89efa70f2e19764dd9064fda6e0646a58a5
AkhlaqMergekart <akhlaq@mergekart.com>
2025-12-17 11:54:42 +0530

    cc

--- index.js
+++ index.js
@@ -312,7 +312,8 @@ async function productMatching(brands, projectId, category) {
                             'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3',
                             'X-API-Key': "test123#"
                         },
-                        data: jsonBody
+                        data: jsonBody,
+                        timeout: 120000
                     };
 
                     const matchResponse = await axios.request(config);
