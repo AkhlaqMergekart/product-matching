4e7134569e2cf8e0317736f4b6fa688591b6b0f4
AkhlaqMergekart <akhlaq@mergekart.com>
2026-06-05 15:32:44 +0530

    cc

--- index.js
+++ index.js
@@ -108,7 +108,7 @@ async function productMatching(brands, projectId, category) {
             await page.setUserAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36");
 
             try {
-                await page.goto(url, { waitUntil: 'networkidle2', timeout: 60000 });
+                await page.goto(url, { waitUntil: 'networkidle2', timeout: 120000 });
             } catch (err) {
                 console.error("Error navigating to URL:", err);
                 retryCount++;
